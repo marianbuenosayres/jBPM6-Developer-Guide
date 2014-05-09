855fb763600ba3a2d6ad8f94ee143a3ba670b69a
marianbuenosayres <marianbuenosayres@gmail.com>
2014-05-09 04:20:43 -0300

    Added testAfterRule()

--- chapter-09/process-cep-examples/src/test/java/com/wordpress/marianbuenosayres/proccep/ProcessCEPTest.java
+++ chapter-09/process-cep-examples/src/test/java/com/wordpress/marianbuenosayres/proccep/ProcessCEPTest.java
@@ -1,6 +1,8 @@
 package com.wordpress.marianbuenosayres.proccep;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -13,6 +15,7 @@ import org.kie.api.KieBase;
 import org.kie.api.KieBaseConfiguration;
 import org.kie.api.KieServices;
 import org.kie.api.conf.EventProcessingOption;
+import org.kie.api.event.rule.AfterMatchFiredEvent;
 import org.kie.api.event.rule.DefaultAgendaEventListener;
 import org.kie.api.event.rule.RuleFlowGroupActivatedEvent;
 import org.kie.api.runtime.KieSession;
@@ -72,8 +75,64 @@ public class ProcessCEPTest {
 		
 		Assert.assertEquals(1, mockNotifierService.getWarnings().size());
 		String warning = mockNotifierService.getWarnings().iterator().next();
-		Assert.assertEquals("more than reqPriorityProcesses 10 processes in 5 minutes", warning);
+		Assert.assertEquals("reqPriorityProcess: more than 10 processes in 5 minutes", warning);
 
+		ksession.dispose();
+	}
+	
+	@Test
+	public void testAfterRule() throws Exception {
+		KieServices ks = KieServices.Factory.get();
+		KieBaseConfiguration kbconf = ks.newKieBaseConfiguration();
+		//configuration to use CEP
+		kbconf.setOption(EventProcessingOption.STREAM);
+		KieBase kbase = ks.getKieClasspathContainer().newKieBase(kbconf);
+		
+		KieSessionConfiguration ksconf = ks.newKieSessionConfiguration();
+		//configuration to control time speed
+		ksconf.setOption(ClockTypeOption.get(ClockType.PSEUDO_CLOCK.getId()));
+		final KieSession ksession = kbase.newKieSession(ksconf, null);
+		TestAsyncWorkItemHandler handler = new TestAsyncWorkItemHandler();
+		//register the work item handlers needed for the process executions
+		ksession.getWorkItemManager().registerWorkItemHandler("asyncTask", handler);
+		//add the RuleAwareProcessEventLister to put process instances in the rules memory
+		ksession.addEventListener(new RuleAwareProcessEventLister());
+		//Add list to keep track of fired rules
+		final List<String> executions = new ArrayList<String>();
+		ksession.addEventListener(new DefaultAgendaEventListener() {
+			@Override
+			public void afterMatchFired(AfterMatchFiredEvent event) {
+				String ruleName = event.getMatch().getRule().getName();
+				executions.add(ruleName);
+			}
+		});
+
+		//adding something to an entry point
+		ksession.getEntryPoint("tempop-test").insert(new Object());
+		
+		SessionPseudoClock clock = ksession.getSessionClock();
+		Map<String, Object> params1 = new HashMap<String, Object>();
+		Requirement req1 = new Requirement("req1", "Requirement 1");
+		params1.put("req", req1);
+		ksession.startProcess("reqPriorityProcess", params1);
+		clock.advanceTime(50, TimeUnit.SECONDS);
+		Map<String, Object> params2 = new HashMap<String, Object>();
+		Requirement req2 = new Requirement("req2", "Requirement 2");
+		params2.put("req", req2);
+		ksession.startProcess("reqPriorityProcess", params2);
+		ksession.fireAllRules();
+		
+		Assert.assertFalse(executions.contains("two processes at least 3 minutes appart"));
+		
+		clock.advanceTime(4, TimeUnit.MINUTES);
+		Map<String, Object> params3 = new HashMap<String, Object>();
+		Requirement req3 = new Requirement("req3", "Requirement 3");
+		params3.put("req", req3);
+		ksession.startProcess("reqPriorityProcess", params3);
+		ksession.fireAllRules();
+
+		Assert.assertTrue(executions.contains("two processes at least 3 minutes appart"));
+		
 		ksession.dispose();
 	}
 }
